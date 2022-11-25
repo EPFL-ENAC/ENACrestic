6ab6c42532ed9d59831e43145b0fba2a514ae9a1
Samuel Bancal <Samuel.Bancal@epfl.ch>
2022-11-25 13:23:27 +0100

    feat(repo lock): auto-unlock when locked since 30+ minutes
    FIX #6
    FIX #10

--- src/enacrestic/restic_backup.py
+++ src/enacrestic/restic_backup.py
@@ -29,6 +29,7 @@ class ResticBackup:
         self._load_env_variables()
         self.current_utc_dt_starting = None
         self.p = None
+        self.need_to_unlock = False
 
     def run(self):
         if not self.app.state.want_to_backup():
@@ -51,6 +52,7 @@ class ResticBackup:
             os.kill(self.p.processId(), signal.SIGINT)
 
     def _run_next_operation(self):
+        self.need_to_unlock = False
         next_operation = self.app.state.next_operation()
         self.app.qt_app.update_system_tray()
         if next_operation is None:
@@ -59,6 +61,8 @@ class ResticBackup:
             self._run_backup()
         elif next_operation == Operation.FORGET:
             self._run_forget()
+        elif next_operation == Operation.UNLOCK:
+            self._run_unlock()
 
     def _load_env_variables(self):
         """
@@ -66,6 +70,10 @@ class ResticBackup:
         to be used by restic commands
         """
         self.env = QProcessEnvironment.systemEnvironment()
+
+        # Be sure to get output messages in english
+        self.env.insert("LC_ALL", "C")
+
         variables_i_search = [
             r"RESTIC_\S+",
             r"AWS_ACCESS_KEY_ID",
@@ -131,6 +139,16 @@ class ResticBackup:
         ]
         self._run(cmd, args)
 
+    def _run_unlock(self):
+        self.app.logger.write_new_date_section("Running restic unlock!")
+        cmd = "restic"
+        args = [
+            "unlock",
+            "--password-file",
+            const.RESTIC_USER_PREFS["PASSWORDFILE"],
+        ]
+        self._run(cmd, args)
+
     def _run(self, cmd, args):
         self.p = QProcess()
         self.p.setProcessEnvironment(self.env)
@@ -149,11 +167,22 @@ class ResticBackup:
     def _handle_stderr(self):
         data = self.p.readAllStandardError()
         stderr = bytes(data).decode("utf8")
+        self.app.logger.error(stderr)
         if re.search(r"timeout", stderr):
             self.current_process_completion_status = ResticCompletionStatus.TIMEOUT
-        if re.search(r"Fatal: unable to create lock", stderr):
+        if re.search(r"the `unlock` command can be used to remove stale locks", stderr):
             self.current_process_completion_status = ResticCompletionStatus.REPO_LOCKED
-        self.app.logger.error(stderr)
+            lock_ago_str = re.search(r"lock was created at.* \((.*)\)", stderr).group(1)
+            lock_hours_bool = re.search(r"\dh\d", lock_ago_str) is not None
+            try:
+                lock_minutes_int = int(re.search(r"(\d+)m", lock_ago_str).group(1))
+            except AttributeError:
+                lock_minutes_int = 0
+            self.need_to_unlock = lock_hours_bool or lock_minutes_int > 30
+            if not self.need_to_unlock:
+                self.app.logger.write(
+                    "The lock is too new. We expect 30+ minutes to unlock manually."
+                )
 
     def _handle_state(self, proc_state):
         if proc_state == QProcess.Starting:
@@ -175,12 +204,20 @@ class ResticBackup:
                     == ResticCompletionStatus.TIMEOUT
                 ):
                     completion_status = Status.NO_NETWORK
+                elif (
+                    self.current_process_completion_status
+                    == ResticCompletionStatus.REPO_LOCKED
+                ):
+                    completion_status = Status.REPO_LOCKED
                 else:
                     completion_status = Status.LAST_OPERATION_FAILED
         else:
             completion_status = Status.LAST_OPERATION_FAILED
         self.app.state.finished_restic_cmd(
-            completion_status, self.current_utc_dt_starting, self.current_chrono
+            completion_status,
+            self.current_utc_dt_starting,
+            self.current_chrono,
+            self.need_to_unlock,
         )
         self.app.logger.write(
             f"Process finished ({exitCode}) in "
